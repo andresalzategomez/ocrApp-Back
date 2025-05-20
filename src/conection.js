de2830407179add0a0bcd94d88f5e73e5352f4ef
aalzate <aalzate@ospinternational.com>
2025-05-19 22:49:28 -0500

    comentar conection old

--- src/conection.js
+++ src/conection.js
@@ -1,19 +1,19 @@
-const Sequelize = require('sequelize') // metodo para conexión rápida a la base de datos
-const path = process.env.DB_URI // cadena de conexión a Base de datos
-const nodemailer = require("nodemailer");
+// const Sequelize = require('sequelize') // metodo para conexión rápida a la base de datos
+// // const path = process.env.DB_URI // cadena de conexión a Base de datos
+// const nodemailer = require("nodemailer");
 
-const sequelize = new Sequelize(path, {
-    dialect: 'mysql',
-    operatorsAliases: 0 ,
-    logging: false,
-});
+// // const sequelize = new Sequelize(path, {
+// //     dialect: 'mysql',
+// //     operatorsAliases: 0 ,
+// //     logging: false,
+// // });
 
-// module.exports = connectDB;
-sequelize.authenticate()
-    .then(() => {
-        console.log('Conectado a Base de Datos.');
-    }).catch(err => {
-        console.error('Error de conexion:', err);
-    })
+// // module.exports = connectDB;
+// sequelize.authenticate()
+//     .then(() => {
+//         console.log('Conectado a Base de Datos.');
+//     }).catch(err => {
+//         console.error('Error de conexion:', err);
+//     })
 
-module.exports = sequelize;
+// module.exports = sequelize;
